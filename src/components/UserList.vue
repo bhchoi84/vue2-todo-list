f4efe8c174f8622f86dd510c9160e823caa94daa
bhchoi84 <bhchoi84@gmail.com>
2022-12-22 15:55:19 +0900

    mapState, mapActions .. 로 store 정보 사용하는 방법 추가

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -2,23 +2,34 @@
     <div class="textAlignLeft">
         <h1>User List</h1> 
         <ul class="listStyleNone">
-            <li v-for="user in users" :key="user.id">{{user.id}} {{user.name}}</li>
+            <li v-for="user in people" :key="user.id">{{user.id}} {{user.name}}</li>
         </ul>
     </div>
 </template>
 <script>
 
+import { mapState, mapActions } from 'vuex' // store의 state를 가져온다.
+
 export default {
     computed:{
-        users(){
-            return this.$store.state.users
-        }
+        // users(){
+        //     return this.$store.state.users
+        // }
+
+        // ...mapState([
+        //     'users'     // store state get
+        // ])
+
+        ...mapState({
+            people: 'users'
+        })
     },
     methods: {
-        getUsers(){
-            console.log('2 getUsers ===> store call');
-            this.$store.dispatch('getUsers')
-        }
+        ...mapActions(['getUsers'])
+        // getUsers(){
+        //     console.log('2 getUsers ===> store call');
+        //     this.$store.dispatch('getUsers')
+        // }
     },
     created() {
         console.log('1 created');
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -9,6 +9,7 @@
     </div>
 </template>
 <script>
+
 import AddTodo from '@/components/AddTodo.vue'
 import TodoList from '@/components/TodoList.vue'
 import CompletedTodo from '@/components/CompletedTodo.vue'
